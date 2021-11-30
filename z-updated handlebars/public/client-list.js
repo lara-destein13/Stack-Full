41dcc4177df430a97ff4e233c4d37572cd2f8da5
the-electro-punk <88555154+the-electro-punk@users.noreply.github.com>
2021-11-29 18:07:09 -0600

    updated code

--- z-updated handlebars/public/client-list.js
+++ z-updated handlebars/public/client-list.js
@@ -1,8 +1,31 @@
 function editButtonClicked(index) {
   window.location=`/client-edit-page?index=${index}`;
 }
+let deleteBTN = document.getElementById('deleteBTN')
+deleteBTN.addEventListener('click', deleteButtonClicked)
+let clientData = document.getElementById('clientData')
+let clientRow = document.getElementById('clientRow')
+
+function deleteButtonClicked(index) {
+  clientRow.removeChild(clientData.value)
+}
+
 
 function addButtonClicked(index) {
   window.location='/client-add-page';
 }
 
+// let addToDoButton = document.getElementById(‘addToDo’);
+// let toDoContainer = document.getElementById(‘toDoContainer’);
+// let inputField = document.getElementById(‘inputField’);
+// addToDoButton.addEventListener(‘click’, function() {
+//     var paragraph = document.createElement(‘p’);
+//     paragraph.innerHTML = inputField.value;
+//     toDoContainer.appendChild(paragraph);
+//     inputField.value = “”;
+//     paragraph.addEventListener(‘dblclick’, function(){
+//         toDoContainer.removeChild(paragraph);
+//     })
+// })
+// 10:40
+// the last line removes list item when you double click directly on the list item
